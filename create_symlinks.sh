7cfd967209daf2945d9a058330f1aba4eefa2914
cuppajoeman <ccn@cuppajoeman.com>
2024-04-21 01:20:49 -0400

    update .gitignore logic

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ln -sr .clangd ../
 ln -sr .clang-format ../
+# https://github.com/git/git/blob/master/Documentation/RelNotes/2.32.0.txt
+# therefore we can't symlink the gitignore, we copy it 
+cp -n .gitignore ../
