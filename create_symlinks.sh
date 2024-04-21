2d547b99ec8970aa353559761c1c089af56c0319
cuppajoeman <ccn@cuppajoeman.com>
2024-04-21 02:06:51 -0400

    update scripts to work properly

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,3 +4,12 @@ ln -sr .clang-format ../
 # https://github.com/git/git/blob/master/Documentation/RelNotes/2.32.0.txt
 # therefore we can't symlink the gitignore, we copy it 
 cp -n .gitignore ../
+
+# https://stackoverflow.com/a/4749368/6660685
+if grep -Fxq ".gitignore" ../.gitignore
+then
+    echo ".gitignore already present in .gitignore"
+else
+    echo "added .gitignore to .gitignore as it was not present"
+    echo .gitignore >> ../.gitignore
+fi
