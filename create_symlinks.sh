a6a541f6b07b2f181bed5ec1e93d04342e9be14b
cuppajoeman <ccn@cuppajoeman.com>
2024-07-11 23:05:13 -0400

    add more ignores and updates comments in script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 ln -sr .clangd ../
 ln -sr .clang-format ../
+echo "Just copied clang files to parent dir"
 # https://github.com/git/git/blob/master/Documentation/RelNotes/2.32.0.txt
-# therefore we can't symlink the gitignore, we copy it 
-cp -n .gitignore ../
+# therefore we can't symlink the gitignore, we copy it  -n is no clobber so doesn't overwrite the file
+
+if [ -e "../.gitignore" ]; then
+    echo "You already have a gitignore, not clobbering it, if you want to replace it delete the current one first"
+else
+  cp .gitignore ../
+  echo "Just pasted gitignore"
+fi
 
 # https://stackoverflow.com/a/4749368/6660685
 if grep -Fxq ".gitignore" ../.gitignore
 then
     echo ".gitignore already present in .gitignore"
 else
-    echo "added .gitignore to .gitignore as it was not present"
+    echo "added .gitignore to .gitignore as it was not present, this is required because the content of the
+    gitignore is stored in clang_formatting not in this project"
     echo .gitignore >> ../.gitignore
 fi
